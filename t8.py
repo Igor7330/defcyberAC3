c77425161f398018360e72f44a36b1ad6d03ccc5
Igor Lopes de Sousa <1903266@salas.aulas>
2020-10-20 20:08:37 -0300

    ac4

--- t8.py
+++ t8.py
@@ -1,18 +1,38 @@
-importar  os
-from  flask  import  Flask , jsonify , request
-de  matemática  import  sqrt
+import os
+from flask import  Flask, jsonify, request
+from math  import  sqrt
 
-app  =  Flask ( __name__ )
+app  =  Flask( __name__ )
 
-@ app . rota ( '/' )
+@ app.route ( '/' )
 def  nao_entre_em_panico ():
 
 
-    primos  =  "Tudo vai dar certo, caros alunos!"
+    limite = 100
+
+    c = 1
+    p = 1
+    numero = 3
+
+    primos = "2."
+
+    while p < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1
+            if(p % 10 == 0):
+                primos = primos + "<br>"
+        numero+=1
 
 
-    voltar  primos
+    return  primos
 
 if  __name__  ==  "__main__" :
-    porta  =  int ( os . amb . get ( "PORT" , 5000 ))
-    app . executar ( host = '0.0.0.0' , porta = porta )
+    port  =  int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
+
